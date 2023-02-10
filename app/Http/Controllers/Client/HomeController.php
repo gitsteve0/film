6675bfede0b9da05ec889280e06a646e51f5ab98
Mekanchik11 <mekannazarov18@gmail.com>
2023-02-10 17:00:08 +0500

    video streamer

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -9,10 +9,10 @@ use Iman\Streamer\VideoStreamer;
 class HomeController extends Controller
 {
     public function index(){
-        $video_path = 'my_video_path';
-
-        $tmp = new \App\VideoStream($video_path);
-        $tmp->start();
+//        $video_path = 'my_video_path';
+//
+//        $tmp = new \App\VideoStream($video_path);
+//        $tmp->start();
 //        $path = public_path('video.vid.mp4');
 //        VideoStreamer::streamFile($path);
         return view('client.home.index');
